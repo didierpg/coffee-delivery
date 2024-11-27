e22a9286b83de25ecb1001e846040f8aceb4e650
Didier Ferreira <didierpg@gmail.com>
2024-11-27 10:31:24 -0300

    feat: configure React Router with BrowserRouter in main component. closes #5

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { App } from './App'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
